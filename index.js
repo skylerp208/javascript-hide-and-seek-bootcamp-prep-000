0d2a62bed74acb12adcff587c3b13c3220416f9c
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-08-23 19:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget(){
 function increaseRankBy(n) {
 let things = document.getElementById('app').querySelectorAll('ul.ranked-list li ')
   for (let i=0; i < things.length; i++) {
-    things[i].innerHTML = parseInt(things[i].innerHTML) + n.tostring()
+    things[i].innerHTML = (parseInt(things[i].innerHTML) + n).toString()
   } 
 }
 
