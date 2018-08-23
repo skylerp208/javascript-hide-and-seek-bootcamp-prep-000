548d07cb47ef6f9de81b60c2c8938715965fb513
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-08-23 19:15:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+ return document.querySelector('#nested').querySelector('.target')
+}
 
+function increaseRankBy(n) {
+let things = document.getElementById('app').querySelectorAll('ul.ranked-list li ')
+  for (let i=0; i < things.length; i++) {
+    things[i].innerHTML = (parseInt(things[i].innerHTML) + n).toString()
+  } 
+}
+
+function deepestChild() {
+ return document.querySelector('#grand-node').querySelector('div').querySelector('div').querySelector('div').querySelector('div')
+}
